dc1eb0dc0e64bb252e7b35c5ce9e9f9a8ce9a7a0
Wladimir <weizen.w@gmx.de>
2023-06-05 16:03:46 +0200

    feat: add improvements

--- src/Task1.java
+++ src/Task1.java
@@ -6,14 +6,13 @@ public class Task1 {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    List<Integer> height = new ArrayList<>();
     int place = 1;
-    System.out.println("          =  =  =  =  =  МЕСТО ИВАНА В СТРОЮ  =  =  =  =  =");
+    System.out.println("         =  =  =  =  =  МЕСТО ИВАНА В СТРОЮ  =  =  =  =  =");
     System.out.println("Введите рост учеников (натуральное число, не более 200) по убыванию:");
     System.out.println("(в конце укажите любой символ, отличный от числа, для завершения ввода)");
-    List<Integer> height = new ArrayList<>();
     while (scanner.hasNextInt()) {
-      int x = scanner.nextInt();
-      height.add(x);
+      height.add(scanner.nextInt());
     }
     scanner.next();
     System.out.println("Введите рост Ивана (натуральное число, не более 200):");
